93d008dbed9dc398597017da9c047b5f3b50b6a2
Orynik <dkrovel@gmail.com>
2020-09-02 12:08:42 +0300

    Создание проектов для сущностей, MVC и для Unit-тестирования

---
+++ UnitTes/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTes")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTes")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("314cf89a-78c7-491f-9de2-428da943deda")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,64 @@
+﻿using Domain;
+using Domain.Entities;
+using Moq;
+using Ninject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebUI.Infrastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel kernel;
+
+        public NinjectDependencyResolver(IKernel kernelParam)
+        {
+            kernel = kernelParam;
+            AddBindings();
+        }
+
+        private void AddBindings()
+        {
+
+            Mock<IBookRepository> mock = new Mock<IBookRepository>();
+            mock.Setup(m => m.Books).Returns(new List<Book>
+            {
+                new Book
+                {
+                    Name = "ewqeqweqwe",
+                    Author = "Ya",
+                    Price = 20,
+                    Description = "Opisanie",
+                },
+                new Book
+                {
+                    Name = "ttwet",
+                    Author = "Ya",
+                    Price = 10000,
+                    Description = "Descr",
+                },
+                new Book
+                {
+                    Name = "qweqwe",
+                    Author = "Ya",
+                    Price = 30000000,
+                    Description = "Opisanie",
+                },
+            });
+            kernel.Bind<IBookRepository>().ToConstant(mock.Object);
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return kernel.GetAll(serviceType);
+        }
+    }
+}
