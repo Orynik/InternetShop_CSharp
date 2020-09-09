05d381b0a807329ac84a942ab5a17c8becb48a03
Orynik <dkrovel@gmail.com>
2020-09-09 16:11:33 +0800

    Добавлены кнопки, с помощью которых можно фильтровать книги по категориям

--- UnitTes/UnitTest1.cs
+++ UnitTes/UnitTest1.cs
@@ -113,5 +113,51 @@ namespace UnitTes
             Assert.IsTrue(result[1].Id == 3 && result[1].Name == "Book3" && result[1].Genre == "Genre1");
             Assert.IsTrue(result[2].Id == 5 && result[2].Name == "Book5" && result[2].Genre == "Genre1");
         }
+        [TestMethod]
+        public void Can_Create_Categories()
+        {
+            Mock<IBookRepository> mock = new Mock<IBookRepository>();
+
+            mock.Setup(m => m.Books).Returns(new List<Book>
+            {
+                new Book{Id = 1, Name = "Book1",Genre = "Genre1"},
+                new Book{Id = 2, Name = "Book2",Genre = "Genre2"},
+                new Book{Id = 3, Name = "Book3",Genre = "Genre3"},
+                new Book{Id = 4, Name = "Book4",Genre = "Genre2"},
+                new Book{Id = 5, Name = "Book5",Genre = "Genre1"}
+            });
+
+            NavController target = new NavController(mock.Object);
+
+            List<string> result = ((IEnumerable<string>)target.Menu().Model).ToList();
+
+            Assert.AreEqual(result.Count(), 3);
+            Assert.AreEqual(result[0], "Genre1");
+            Assert.AreEqual(result[1], "Genre2");
+            Assert.AreEqual(result[2], "Genre3");
+        }
+
+        [TestMethod]
+        public void Can_View_CurrentGenre()
+        {
+            Mock<IBookRepository> mock = new Mock<IBookRepository>();
+
+            mock.Setup(m => m.Books).Returns(new List<Book>
+            {
+                new Book{Id = 1, Name = "Book1",Genre = "Genre1"},
+                new Book{Id = 2, Name = "Book2",Genre = "Genre2"},
+                new Book{Id = 3, Name = "Book3",Genre = "Genre3"},
+                new Book{Id = 4, Name = "Book4",Genre = "Genre2"},
+                new Book{Id = 5, Name = "Book5",Genre = "Genre1"}
+            });
+
+            NavController target = new NavController(mock.Object);
+
+            string currentGenre = "Genre1";
+
+            string result = target.Menu(currentGenre).ViewBag.SelectedGenre;
+
+            Assert.AreEqual(currentGenre, result);
+        }
     }
 }
